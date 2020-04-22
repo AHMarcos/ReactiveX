fe9437bb12af85ba3629ae9ba8987a05ebcb0929
AHMarcos <marcos.alarcon@vallegrande.edu.pe>
2020-04-21 21:54:42 -0500

    nuevos temas

---
+++ src/transformacion/06-exhaustMap.ts
@@ -0,0 +1,20 @@
+import { interval, fromEvent } from 'rxjs';
+import { take, concatMap, exhaustMap } from 'rxjs/operators';
+
+const interval$ = interval(500).pipe(take(3));
+
+const click$ = fromEvent(document,'click');
+
+click$.pipe(
+    exhaustMap( () => interval$),
+    )
+.subscribe(console.log);
+
+
+
+
+
+
+
+
+
